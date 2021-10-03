71d59869de1d7a3e688a9cfc1ff53402de0e5484
mikhailBrann <alunos13@yandex.ru>
2021-10-04 01:14:57 +0300

    add README

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os,requests
-from requests.utils import requote_uri
 
 class YaUploader:
     def __init__(self, token: str):
@@ -10,14 +9,6 @@ class YaUploader:
             'Content-Type': 'application/json',
             'Authorization': f'OAuth {self.token}'
         }
-
-    def get_files_list(self, ya_cloud_url='https://cloud-api.yandex.net/v1/disk/resources/files/'):
-        headers = self.get_headers()
-        response = requests.get(url=ya_cloud_url, headers=headers)
-        if response.status_code == 200:
-            return response.json()
-        else:
-            return f'Ошибка, код ответа: {response.status_code}'
 
     def _get_upload_link(self, disk_file_path):
         upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
@@ -32,13 +23,13 @@ class YaUploader:
             return False
 
     def upload(self, file_path: str='', hard_disk_dir='files'):
-        """Метод загружает файлы по списку file_list на яндекс диск"""
+        """Метод загружает файлы из директории {hard_disk_dir} на яндекс диск"""
+
         os_separator = os.path.sep
         path_to_file = hard_disk_dir + os_separator
     
         # получаем все файлы из директории {hard_disk_dir} и записываем в список
-        with os.scandir(path_to_file) as file_list:
-             
+        with os.scandir(path_to_file) as file_list:          
             for file_ in file_list:
                 if file_.is_file():
                     response = self._get_upload_link(file_path + file_.name)
@@ -55,7 +46,6 @@ class YaUploader:
                     else:
                         print(f'Ошибка: директории {file_path} нет в облачном хранилище.\nУкажите существующую директорию или оставьте поле пустым для загрузки в корень хранилища')
                         break
-
 
 
 if __name__ == '__main__':
